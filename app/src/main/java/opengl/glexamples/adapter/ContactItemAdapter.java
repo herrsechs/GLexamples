bd9df71bd03dee06d5e0b8574768683cb9af5227
杨安琪 <yangllzz01@163.com>
2015-12-24 01:36:55 +0800

    Search user in contactList

--- app/src/main/java/opengl/glexamples/adapter/ContactItemAdapter.java
+++ app/src/main/java/opengl/glexamples/adapter/ContactItemAdapter.java
@@ -30,83 +30,75 @@ public class ContactItemAdapter extends RecyclerView.Adapter<ContactItemAdapter.
 
     private final LayoutInflater layoutInflater;
     private final Context context;
-    private ArrayList<String> usernames=new ArrayList<>();
-    private static final String TAG = "ContactAdapter";
-
-    public static String[] CONTACT_PROJECTION=new String[]{
-            ContactsContract.Contacts._ID,
-            ContactsContract.Contacts.DISPLAY_NAME
-    };
+    private ArrayList<String> usernames;
 
 
-    public ArrayList<String > getContact(){
-
-        ArrayList<String> usernames=new ArrayList<>();
-
-        Cursor cursor = context.getContentResolver().query(ContactsContract.Contacts.CONTENT_URI,
-                null, null, null, null);
-        Cursor phones = null;
-        Cursor emails=  null;
-        int contactIdIndex = 0;
-        int nameIndex = 0;
-
-        if(cursor.getCount() > 0) {
-            contactIdIndex = cursor.getColumnIndex(ContactsContract.Contacts._ID);
-            nameIndex = cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME);
-        }
-
-
-        while(cursor.moveToNext()) {
-            String contactId = cursor.getString(contactIdIndex);
-            String name = cursor.getString(nameIndex);
-
-            Log.i(TAG, contactId);
-            Log.i(TAG, name);
-            usernames.add(name);
+//    public ArrayList<String > getContact(){
+//
+//        ArrayList<String> usernames=new ArrayList<>();
+//
+//        Cursor cursor = context.getContentResolver().query(ContactsContract.Contacts.CONTENT_URI,
+//                null, null, null, null);
+//        Cursor phones = null;
+//        Cursor emails=  null;
+//        int contactIdIndex = 0;
+//        int nameIndex = 0;
+//
+//        if(cursor.getCount() > 0) {
+//            contactIdIndex = cursor.getColumnIndex(ContactsContract.Contacts._ID);
+//            nameIndex = cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME);
+//        }
+//
+//
+//        while(cursor.moveToNext()) {
+//            String contactId = cursor.getString(contactIdIndex);
+//            String name = cursor.getString(nameIndex);
+//            usernames.add(name);
 
             /*
              * 查找该联系人的phone信息
              */
-            phones = context.getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
-                    null,
-                    ContactsContract.CommonDataKinds.Phone.CONTACT_ID + "=" + contactId,
-                    null, null);
-            int phoneIndex = 0;
-            if(phones.getCount() > 0) {
-                phoneIndex = phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
-            }
-            while(phones.moveToNext()) {
-                String phoneNumber = phones.getString(phoneIndex);
-                Log.i(TAG, phoneNumber);
-            }
+//            phones = context.getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+//                    null,
+//                    ContactsContract.CommonDataKinds.Phone.CONTACT_ID + "=" + contactId,
+//                    null, null);
+//            int phoneIndex = 0;
+//            if(phones.getCount() > 0) {
+//                phoneIndex = phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+//            }
+//            while(phones.moveToNext()) {
+//                String phoneNumber = phones.getString(phoneIndex);
+//                Log.i(TAG, phoneNumber);
+//            }
 
             /*
              * 查找该联系人的email信息
              */
-            emails = context.getContentResolver().query(ContactsContract.CommonDataKinds.Email.CONTENT_URI,
-                    null,
-                    ContactsContract.CommonDataKinds.Email.CONTACT_ID + "=" + contactId,
-                    null, null);
-            int emailIndex = 0;
-            if(emails.getCount() > 0) {
-                emailIndex = emails.getColumnIndex(ContactsContract.CommonDataKinds.Email.DATA);
-            }
-            while(phones.moveToNext()) {
-                String email = emails.getString(emailIndex);
-                Log.i(TAG,email);
-            }
-        }
-        cursor.close();
-        phones.close();
-        emails.close();
-        return usernames;
-    }
+//            emails = context.getContentResolver().query(ContactsContract.CommonDataKinds.Email.CONTENT_URI,
+//                    null,
+//                    ContactsContract.CommonDataKinds.Email.CONTACT_ID + "=" + contactId,
+//                    null, null);
+//            int emailIndex = 0;
+//            if(emails.getCount() > 0) {
+//                emailIndex = emails.getColumnIndex(ContactsContract.CommonDataKinds.Email.DATA);
+//            }
+//            while(phones.moveToNext()) {
+//                String email = emails.getString(emailIndex);
+//                Log.i(TAG,email);
+//            }
+      //  }
+     //   cursor.close();
+//        phones.close();
+//        emails.close();
+
+//        return usernames;
+//    }
 
 
-    public ContactItemAdapter(Context context) {
+    public ContactItemAdapter(Context context,ArrayList<String> usernames) {
         this.context = context;
         this.layoutInflater = LayoutInflater.from(context);
-        this.usernames=getContact();
+        this.usernames=usernames;
     }
 
     public int getItemCount(){
--- app/src/main/java/opengl/glexamples/glActivity/DisplayContactActivity.java
+++ app/src/main/java/opengl/glexamples/glActivity/DisplayContactActivity.java
@@ -2,15 +2,21 @@ package opengl.glexamples.glActivity;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -23,19 +29,41 @@ import opengl.glexamples.adapter.ContactItemAdapter;
  */
 public class DisplayContactActivity extends Activity{
     public RecyclerView contactRecyclerView;
+    public EditText inputSearchQuery;
+    private ArrayList<String> usernames;
+    LinearLayoutManager linearLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        usernames=getUsernames(this);
+        linearLayoutManager=new LinearLayoutManager(this);
         setContentView(R.layout.layout_display_contact);
-
-        Intent intent=getIntent();
 
 
         contactRecyclerView = (RecyclerView)findViewById(R.id.contact_recycler_view);
-        contactRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-        contactRecyclerView.setAdapter(new ContactItemAdapter(this));
+        contactRecyclerView.setLayoutManager(linearLayoutManager);
+        contactRecyclerView.setAdapter(new ContactItemAdapter(this, usernames));
 
+        inputSearchQuery=(EditText)findViewById(R.id.input_search_query);
+        inputSearchQuery.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String username = inputSearchQuery.getText().toString();
+                searchAndDisplayUser(username);
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
 
     }
 
@@ -61,6 +89,49 @@ public class DisplayContactActivity extends Activity{
         return super.onOptionsItemSelected(item);
     }
 
+    public ArrayList<String> getUsernames(Context context){
+        ArrayList<String> usernames=new ArrayList<>();
 
+        Cursor cursor = context.getContentResolver().query(ContactsContract.Contacts.CONTENT_URI,
+                null, null, null, null);
+        //int contactIdIndex = 0;
+        int nameIndex = 0;
+
+        if(cursor.getCount() > 0) {
+            //contactIdIndex = cursor.getColumnIndex(ContactsContract.Contacts._ID);
+            nameIndex = cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME);
+        }
+
+
+        while(cursor.moveToNext()) {
+            //String contactId = cursor.getString(contactIdIndex);
+            String name = cursor.getString(nameIndex);
+            usernames.add(name);
+        }
+        return usernames;
+    }
+
+    public int findUserByName(String username){
+        int idx=0;
+        for(;idx<usernames.size();idx++){
+            // Find User
+            if(usernames.get(idx).equals(username)) break;
+        }
+        //Cannot Find User
+        if(idx==usernames.size()) idx=-1;
+        return idx;
+    }
+
+    public void searchAndDisplayUser(String username){
+        int idx=findUserByName(username);
+
+        if(-1==idx){
+                    //找不到联系人
+        }
+
+        //找到联系人
+        linearLayoutManager.scrollToPositionWithOffset(idx,20);
+
+    }
 
 }
